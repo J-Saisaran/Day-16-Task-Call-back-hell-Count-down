badd199ef576cbe5097e13196e0472837218ff29
J-Saisaran <167777747+J-Saisaran@users.noreply.github.com>
2024-05-23 18:00:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,49 +2,49 @@ var parent_div=document.createElement("div");
 parent_div.className="main";
 
 
-print_data(display);
+print_number(TextHID);
 
 document.body.append(parent_div);
 
 
-function print_data(callback){
+function print_number(callback){
 
     setTimeout(()=>{
-        parent_div.innerHTML="10";
+        parent_div.innerHTML="Countdown starts:10";
     },1000);
     setTimeout(()=>{
-        parent_div.innerHTML="9";
+        parent_div.innerHTML="Countdown starts:9";
     },2000);
     setTimeout(()=>{
-        parent_div.innerHTML="8";
+        parent_div.innerHTML="Countdown starts:8";
     },3000);
     setTimeout(()=>{
-        parent_div.innerHTML="7";
+        parent_div.innerHTML="Countdown starts:7";
     },4000);
     setTimeout(()=>{
-        parent_div.innerHTML="6";
+        parent_div.innerHTML="Countdown starts:6";
     },5000);
     setTimeout(()=>{
-        parent_div.innerHTML="5";
+        parent_div.innerHTML="Countdown starts:5";
     },6000);
     setTimeout(()=>{
-        parent_div.innerHTML="4";
+        parent_div.innerHTML="Countdown starts:4";
     },7000);
     setTimeout(()=>{
-        parent_div.innerHTML="3";
+        parent_div.innerHTML="Countdown starts:3";
     },8000);
     setTimeout(()=>{
-        parent_div.innerHTML="2";
+        parent_div.innerHTML="Countdown starts:2";
     },9000);
     setTimeout(()=>{
-        parent_div.innerHTML="1";
+        parent_div.innerHTML="Countdown starts:1";
     },10000);
     callback("Happy Independence Day")
     
 
 }
 
-function display(data){
+function TextHID(data){
     setTimeout(()=>{
         parent_div.innerHTML=data;
     },11000);
