39ea666786395b627046468f1e617a1349b18eeb
J-Saisaran <167777747+J-Saisaran@users.noreply.github.com>
2024-05-23 15:00:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,51 @@
-var parent = document.createElement("div");
-parent.className = "main";
+var parent_div=document.createElement("div");
+parent_div.className="main";
 
-document.body.append(parent);
 
-function day(data) {
-    setTimeout(() => {
-        parent.innerHTML = data;
-    }, 10000);
+print_data(display);
+
+document.body.append(parent_div);
+
+
+function print_data(callback){
+
+    setTimeout(()=>{
+        parent_div.innerHTML="10";
+    },1000);
+    setTimeout(()=>{
+        parent_div.innerHTML="9";
+    },2000);
+    setTimeout(()=>{
+        parent_div.innerHTML="8";
+    },3000);
+    setTimeout(()=>{
+        parent_div.innerHTML="7";
+    },4000);
+    setTimeout(()=>{
+        parent_div.innerHTML="6";
+    },5000);
+    setTimeout(()=>{
+        parent_div.innerHTML="5";
+    },6000);
+    setTimeout(()=>{
+        parent_div.innerHTML="4";
+    },7000);
+    setTimeout(()=>{
+        parent_div.innerHTML="3";
+    },8000);
+    setTimeout(()=>{
+        parent_div.innerHTML="2";
+    },9000);
+    setTimeout(()=>{
+        parent_div.innerHTML="1";
+    },10000);
+    callback("Happy Independence Day")
+    
 
 }
 
-function data(callback) {
-    var x = 0;
-    for (let i = 10; i > 0; i--) {
-        setTimeout(() => {
-            parent.innerHTML = `Count down starts: ${i}`;
-        }, x);
-        x += 1000;
-    }
-    callback("Happy Independence Day");
+function display(data){
+    setTimeout(()=>{
+        parent_div.innerHTML=data;
+    },11000);
 }
-data(day);
